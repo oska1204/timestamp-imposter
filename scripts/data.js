6ca32076b5974ae9fb09e97b01915bfbf5717db1
oska1204 <oska1204@edu.campusvejle.dk>
2022-04-01 09:03:31 +0200

    edits heading

--- scripts/data.js
+++ scripts/data.js
@@ -1,6 +1,6 @@
 elmItem.addEventListener('send-data', e => {
     const { json } = e.detail
-    preJson.textContent = JSON.stringify(json, null, 3)
+    preJson.textContent = JSON.stringify(json, null, 2)
 })
 
 elmItem.update.click()
